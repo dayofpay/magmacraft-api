dd5e993c9b3c73b346d50970be1a74141e87b069
Vladislav <42325421+dayofpay@users.noreply.github.com>
2022-12-21 14:20:27 +0200

    Create parseDateExample.js

---
+++ parseDateExample.js
@@ -0,0 +1,7 @@
+const url = "https://v-devs.online/api.php?projectName=magmacraft&functionName=getClanPlayers&clanName=Gapple";
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    // 
+  });
